374c9b2cd4f8aed46eb0d43cde1cd560a66916e6
Mateusz Uzdowski <mateusz@silverstripe.com>
2011-04-07 17:56:42 +1200

    ENHANCEMENT: test the remaining calls in Poll

--- tests/unit/PollTest.php
+++ tests/unit/PollTest.php
@@ -18,4 +18,32 @@ class PollTest extends SapphireTest {
 		$this->assertContains(urlencode('Android (80)'), $chart);
 		$this->assertContains(urlencode('Other (12)'), $chart);
 	}
+
+	function testMaxVotes() {
+		$mobilePoll = $this->ObjFromFixture('Poll', 'mobile-poll');
+		$this->assertEquals(120, $mobilePoll->maxVotes());	
+	}
+
+	function testVisible() {
+		$mobilePoll = $this->ObjFromFixture('Poll', 'mobile-poll');
+		$this->assertTrue($mobilePoll->getVisible());
+
+		$mobilePoll->IsActive = false;
+		$mobilePoll->write();
+		$this->assertFalse($mobilePoll->getVisible());
+
+		$mobilePoll->IsActive = true;
+		$mobilePoll->Embargo = "2010-10-10 10:10:10";
+		$mobilePoll->Expiry = "2010-10-11 10:10:10";
+		$mobilePoll->write();
+
+		SS_Datetime::set_mock_now('2010-10-10 10:00:00');
+		$this->assertFalse($mobilePoll->getVisible());
+
+		SS_Datetime::set_mock_now('2010-10-10 11:00:00');
+		$this->assertTrue($mobilePoll->getVisible());
+
+		SS_Datetime::set_mock_now('2010-10-12 10:00:00');
+		$this->assertFalse($mobilePoll->getVisible());
+	}
 }
