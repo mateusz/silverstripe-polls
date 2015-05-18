62f871889e913f82d8530a31da6b142779c3e664
matt-in-a-hat <matt@mediasuite.co.nz>
2015-05-19 11:57:55 +1200

    Add subsite support
    By deafult it will be available in the CMS for subsites, and polls will not be shared between subsites.
    Both of these can be changed through config.

--- code/PollAdmin.php
+++ code/PollAdmin.php
@@ -8,5 +8,18 @@ class PollAdmin extends ModelAdmin {
 	static $managed_models = array(
 		'Poll'
 	);
-	
+
+	public function subsiteCMSShowInMenu() {
+		return Config::inst()->get('Poll', 'subsite_cms_show_in_menu');
+	}
+
+	public function getEditForm($id = null, $fields = null) {
+		$form = parent::getEditForm($id, $fields);
+		if (class_exists('Subsite') && !Config::inst()->get('Poll', 'subsites_share_polls')) {
+			$gridField = $form->Fields()->fieldByName($this->sanitiseClassName($this->modelClass));
+			$list = $gridField->getList()->filter(array('SubsiteID' => Subsite::currentSubsiteID()));
+			$gridField->setList($list);
+		}
+		return $form;
+	}
 }
